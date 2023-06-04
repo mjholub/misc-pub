f4e3a5741402406cf219c0c06353355cf13d230d
Marcelina Hołub <mholub@tutanota.com>
2023-06-04 17:18:41 +0200

    style(fish): split long lines in apk TUI

--- fish/functions/apktui.fish
+++ fish/functions/apktui.fish
@@ -1,6 +1,7 @@
 function apktui
     set options Upgrade Install Remove Search
-    set choice (echo $options | tr ' ' '\n' | fzf --no-hscroll --prompt "apk: " --preview "doas apk info {}" --preview-window down:3:wrap)
+    set choice (echo $options | tr ' ' '\n' | fzf --no-hscroll --prompt "apk: " \
+    --preview "doas apk info {}" --preview-window down:3:wrap)
     switch $choice
         case Upgrade
             doas apk upgrade --simulate | $PAGER
@@ -10,9 +11,13 @@ function apktui
             end
         case Install
             doas apk update
-            apk search | sed -E 's/-[0-9].*$//' | fzf -m --preview 'sh -c "apk info -s {} && apk info -r {}"' | xargs -I {} doas apk add {}
+            apk search | sed -E 's/-[0-9].*$//' | fzf -m --preview \
+                'sh -c "apk info -s {} && apk info -r {} && apk info -P {}"' \
+                | xargs -I {} doas apk add {}
         case Remove
-            apk list --installed | sed -E 's/(.*?)-[0-9].*?/\\1/' | fzf -m --preview 'sh -c "apk info -s {} && apk info -r {}"' | xargs -I {} doas apk del {}
+            apk list --installed | sed -E 's/(.*?)-[0-9].*?/\\1/' | fzf -m --preview \
+                'sh -c "apk info -s {} && apk info -r {} apk info -P {}"' \
+                | xargs -I {} doas apk del {}
         case Search
             read -p "Enter the package name to search: " pkg
             apk info $pkg
